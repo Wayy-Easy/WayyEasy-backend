8b34fbad13fb94893d36f9b261883a2e6aaa89f4
shadab <gabuyama68@gmail.com>
2023-06-04 14:11:32 +0530

    wip

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -2,9 +2,49 @@ import AdminPayments from "../../models/admin/adminPayments.js";
 
 export const addPayment = async (req, res) => {
   try {
-    if (req.user.role === "admin") {
+    if (req.user.role === "user") {
       const payments = await AdminPayments.create(req.body);
       res.send(payments);
+    } else res.json({ message: "Only users are allowed to add payment" });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+export const getPaymentLsit = async (req, res) => {
+  try {
+    if (req.user.role === "admin") {
+      const data = await AdminPayments.find();
+      res.send(data);
+    } else res.json({ message: "Only admin is allowed to access this" });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+export const getPaymentFullDetails = async (req, res) => {
+  try {
+    if (req.user.role === "admin") {
+      const data = await AdminPayments.findById(req.body.id).populate({
+        path: "physicianId",
+        select: ["name", "image", "mobile", "price"],
+      });
+      res.send(data);
+    } else res.json({ message: "Only admin is allowed to access this" });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+export const finishPayment = async (req, res) => {
+  try {
+    if (req.user.role === "admin") {
+      const data = await AdminPayments.findByIdAndUpdate(
+        req.body.id,
+        { paymentStatus: "Paid" },
+        { new: true }
+      );
+      res.send(data);
     } else res.json({ message: "Only admin is allowed to access this" });
   } catch (error) {
     res.json({ message: error.message });
--- routes/admin/adminRoutes.js
+++ routes/admin/adminRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { requireSignin } from "../../middlewares/auth.js";
-import { addPayment } from "../../controllers/admin/adminController.js";
+import { requireSignin, verifyUser } from "../../middlewares/auth.js";
+import {
+  addPayment,
+  finishPayment,
+  getPaymentFullDetails,
+  getPaymentLsit,
+} from "../../controllers/admin/adminController.js";
 
 const router = express.Router();
 
-router.route("/").post(requireSignin, addPayment);
+router
+  .route("/")
+  .post(verifyUser, addPayment)
+  .get(requireSignin, getPaymentLsit);
+
+router.route("/fullDetails").get(requireSignin, getPaymentFullDetails);
+
+router.route("/finishPayment").patch(requireSignin, finishPayment);
 
 export default router;
