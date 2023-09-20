5c538c11dbd5e0f280cfdb98c3aefc62908b847f
shadab <gabuyama68@gmail.com>
2023-09-20 15:22:31 +0530

    wip

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.use(cors());
 const PORT = process.env.PORT || 2001;
 
 app.get("/", (req, res) => {
+  console.log(req.headers["authorization"]?.length);
   res.send("WayyEasy server is running successfully.");
 });
 
