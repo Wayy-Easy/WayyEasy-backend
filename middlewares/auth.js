82dbed491fd144585a72465bea5850eb93efdec8
shadab <gabuyama68@gmail.com>
2023-09-20 15:19:08 +0530

    wip

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const requireSignin = (req, res, next) => {
   try {
-    console.log(req.headers["authorization"]);
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if (token) {
-      const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
-      req.user = user;
-      next();
-    } else res.status(400).json({ message: "Admin/owner not found" });
+    if (req.headers["authorization"]) {
+      const token = req.headers["authorization"]?.split(" ")[1];
+      if (token) {
+        const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.user = user;
+        next();
+      } else res.status(400).json({ message: "Admin/owner not found" });
+    } else res.status(400).json({ message: "Authorization is required" });
   } catch (error) {
     res.status(400).json({ message: "Access denied" });
     console.log(error);
@@ -33,12 +34,16 @@ export const verifyUser = (req, res, next) => {
 
 export const doctorsSignin = (req, res, next) => {
   try {
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if (token) {
-      const user = jwt.verify(token, process.env.DOCTORS_SECRET_KEY);
-      req.user = user;
-      next();
-    } else res.status(400).json({ message: "Doctor not found" });
+    if (req.headers["authorization"]) {
+      const token = req.headers["authorization"]?.split(" ")[1];
+      if (token) {
+        const user = jwt.verify(token, process.env.DOCTORS_SECRET_KEY);
+        req.user = user;
+        next();
+      } else res.status(400).json({ message: "Doctor not found" });
+    } else {
+      res.send({ message: "Authorization is required" });
+    }
   } catch (error) {
     res.status(400).json({ message: "Access denied" });
     console.log(error);
